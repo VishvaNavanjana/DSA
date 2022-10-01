0b1ee906d438116233bc08af01283bb35e7d0dea
VishvaNavanjana <vishvanavanjana@gmail.com>
2022-10-01 12:26:53 +0530

    breadth First and Deapthe First search code added

---
+++ Graphs/has-path.js
@@ -0,0 +1,28 @@
+const hasPath = (graph, src, dst) => {
+    const queue = [src];
+
+    while(queue.length > 0){
+        const current = queue.shift();
+
+        if(current === dst) return true;
+
+        for(let neighbor of graph[current]){
+            queue.push(neighbor);
+        }
+    }
+
+    return false;
+};
+
+
+// const hasPath = (graph, src, dst) => {
+//     if (src === dst) return true;
+
+//     for (let neighbor of graph[src]){
+//         if(hasPath(graph, neighbor, dst) === true){
+//             return true;
+//         }
+//     }
+
+//     return false;
+// };
