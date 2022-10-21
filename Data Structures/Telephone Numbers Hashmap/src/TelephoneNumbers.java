ccbc07fa86043af7144db2ef35891778e97ec16b
VishvaNavanjana <vishvanavanjana@gmail.com>
2022-10-21 20:20:31 +0530

    Data input done

--- Data Structures/Telephone Numbers Hashmap/src/TelephoneNumbers.java
+++ Data Structures/Telephone Numbers Hashmap/src/TelephoneNumbers.java
@@ -3,21 +3,56 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
-public class TelephoneNumbers<INTEGER> {
-    private HashMap<INTEGER, ArrayList<String>> map = new HashMap<INTEGER, ArrayList<String>>();
+public class TelephoneNumbers {
+    private HashMap<Integer, ArrayList<String>> map = new HashMap<Integer, ArrayList<String>>();
 
     public TelephoneNumbers() {
+    }
+
+    //hash function
+    public int hash(String TelephoneNumber){
+        int hash=0;
+
+        try{
+            int digit10 = Integer.parseInt(TelephoneNumber.substring(9,10));
+            int digit9 = Integer.parseInt(TelephoneNumber.substring(8,9));
+            int digit8 = Integer.parseInt(TelephoneNumber.substring(7,8));
+
+            int number = digit8*100+digit9*10+digit10;
+            hash = number;
+        }
+        catch(Exception e){
+            System.out.println("Error "+e);
+        }
+
+
+        return hash;
     }
 
     
 
     public static void main(String[] args) throws IOException {
         java.io.BufferedReader in = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
-        System.out.println("Enter a telephone number: ");
-        String input = in.readLine();
+        TelephoneNumbers tn = new TelephoneNumbers();
 
 
+        try {
+            System.out.println("Enter a telephone number: ");
+            String input = in.readLine();
 
+            //check the input whether it is a valid telephone number
+            if(input.length() != 10){
+                System.out.println("Invalid telephone number");
+            }
+            else{
+                int hash = tn.hash(input);
+                System.out.println("Hash value: "+ hash);
+            }
+
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
 
 
     }
