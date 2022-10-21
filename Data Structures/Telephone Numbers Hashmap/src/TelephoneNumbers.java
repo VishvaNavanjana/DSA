e83a4f7c75ec7dc82b5e6f5e52520b5c6546bab4
VishvaNavanjana <vishvanavanjana@gmail.com>
2022-10-21 18:51:11 +0530

    Telephone Hashmap initiated

---
+++ Data Structures/Telephone Numbers Hashmap/src/TelephoneNumbers.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class TelephoneNumbers<INTEGER> {
+    private HashMap<INTEGER, ArrayList<String>> map = new HashMap<INTEGER, ArrayList<String>>();
+
+    public TelephoneNumbers() {
+    }
+
+    
+
+    public static void main(String[] args) throws IOException {
+        java.io.BufferedReader in = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+        System.out.println("Enter a telephone number: ");
+        String input = in.readLine();
+
+
+
+
+
+    }
+
+}
