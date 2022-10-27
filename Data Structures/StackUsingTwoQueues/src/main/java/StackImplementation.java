b42fe19d7a00ae0cf33b1d157da458d7bea854f4
VishvaNavanjana <vishvanavanjana@gmail.com>
2022-10-27 11:08:32 +0530

    stack using two queues code initialized

---
+++ Data Structures/StackUsingTwoQueues/src/main/java/StackImplementation.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackImplementation {
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+
+    public StackImplementation() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int n){
+        q1.add(n);
+    }
+
+}
